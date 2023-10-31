b04ed16079e55db03b892cca0043b1fdc3fcc8db
Anna <annasereno27@gmail.com>
2023-10-31 14:13:25 -0500

    continued exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,35 @@
 public class HelloWorld {
 
     public static void main(String[] args) {//shortcut main+tab
-        System.out.println("Hello World!");//shortcut sout+tab
+        //System.out.println("Hello World!");//shortcut sout+tab
+//        int myFavoriteNumber = 5;
+//        System.out.println(myFavoriteNumber);
+//        String myString = "3.14159";
+//        System.out.println(myString);
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
     }
 
 }
